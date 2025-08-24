ca79c37fca41388ac144a1c5e56356234365496c
paulhectork <paul.kervegan@hotmail.fr>
2025-08-24 20:48:47 +0200

    [TEST] basic structure

--- src/test.py
+++ src/test.py
@@ -7,25 +7,25 @@ from track import Track
 
 class TestEnvelope(unittest.TestCase):
     def setUp(self):
-        print("HELLOOOOOOOOO")
+        print("HELLOOOOOOOOO setUp")
 
     def tearDown(self):
         ...
 
-def load_tests(loader=unittest.TestLoader(), tests=[], pattern=None) -> unittest.TestSuite:
-    # test classes to run should be added here
-    test_cases = [TestEnvelope]
+    def test_xxx(self):
+        print("HELLOOOOOOOOO test_xxx")
 
-    suite = unittest.TestSuite()
-    for t in test_cases:
-        tests = loader.loadTestsFromTestCase(t)
-        suite.addTests(tests)
-    return suite
 
 def runner() -> None:
-    textrunner = unittest.TextTestRunner()
-    suite = load_tests()
-    textrunner.run(suite)
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTests([
+        # add other tests here
+        loader.loadTestsFromTestCase(TestEnvelope)
+    ])
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
     return
 
 
